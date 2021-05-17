616850af618bdc89861418921dc3e6c50119b2e3
Aegide <geoffroygressier1@yahoo.fr>
2021-05-18 01:58:16 +0200

    Update 1.0.5

--- src/main/java/com/aegide/aegidemoddemo/Main.java
+++ src/main/java/com/aegide/aegidemoddemo/Main.java
@@ -2,34 +2,71 @@ package com.aegide.aegidemoddemo;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
-
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityFurnace;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
+
+
+
+
+
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main
 {
     public static final String MODID = "aegidemoddemo";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.0.5";
     
     public static Item amethyst;
     public static Block amethystOre;
+    public static Block amethystBlock;
+    
+	ItemStack output;
+	ItemStack input;
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
 		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    	System.out.println("Bonjour");
+        //System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
         
-        amethyst = new ModItem("amethyst");
-        GameRegistry.registerItem(amethyst, "amethyst");
         
-        amethystOre = new ModBlock(Material.rock, "amethystOre", amethyst);
-        GameRegistry.registerBlock(amethystOre, "amethystOre");
+        
+        String amethystName = "amethyst";
+        amethyst = new ModItem(amethystName);
+        GameRegistry.registerItem(amethyst, amethystName);
+        
+        String amethystOreName = "amethystOre";
+        amethystOre = new ModOre(Material.rock, amethystOreName, amethyst);
+        GameRegistry.registerBlock(amethystOre, amethystOreName);
+        
+        String amethystBlockName = "amethystBlock";
+        amethystBlock = new ModBlock(Material.iron, amethystBlockName, amethyst);
+        GameRegistry.registerBlock(amethystBlock, amethystBlockName);
+        
+        
+        
+        output = new ItemStack(amethyst, 9);
+        GameRegistry.addShapelessRecipe(output,
+				new ItemStack(amethystBlock, 1)
+		);
+        
+        output = new ItemStack(amethystBlock);
+        GameRegistry.addRecipe(output,
+				"aaa", "aaa", "aaa",
+				'a', amethyst
+		);
+        
+        input = new ItemStack(amethystOre);
+        output = new ItemStack(amethyst);
+        GameRegistry.addSmelting(input, output, 1);
+        
+        
         
         GameRegistry.registerWorldGenerator(new ModWorldGenerator(amethystOre, 6), 0);
     }
--- src/main/java/com/aegide/aegidemoddemo/ModOre.java
+++ src/main/java/com/aegide/aegidemoddemo/ModOre.java
@@ -1,24 +1,34 @@
 package com.aegide.aegidemoddemo;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
-public class ModBlock extends Block
+public class ModOre extends Block
 {
-	//private Item toCraft;
+	private Item toDrop;
 			
-	public ModBlock(Material material, String blockName, Item toCraft)
+	public ModOre(Material material, String blockName, Item toDrop)
     {
         super(material);
         this.setBlockName(blockName);
         this.setBlockTextureName(Main.MODID + ":" + blockName);
         this.setCreativeTab(CreativeTabs.tabBlock);
-        this.setHardness(5.0F);
+        this.setHardness(4.0F);
         this.setHarvestLevel("pickaxe", 1);
-        //this.toCraft = toCraft;
+        this.toDrop = toDrop;
     }
 	
-
+	@Override
+    public Item getItemDropped(int i, Random random, int j)
+    {
+        if(toDrop != null)
+        {
+            return toDrop;
+        }
+        else return Item.getItemFromBlock(this);
+    }
 }
